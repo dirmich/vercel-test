317eeccd857ff4fd8b2c279e570d1a641e23fd32
David, Shin <dirmich@gmail.com>
2024-08-27 16:45:55 +0900

    a

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 // console.log('Hello via Bun!')
 
-const server = Bun.serve({
-  port: 3008,
-  fetch(req) {
-    // return new Response('Hello')
-    return Response.json({ msg: 'Hello' })
+import type { Server } from 'bun'
+
+// const server = Bun.serve({
+//   port: 3008,
+//   fetch(req) {
+//     // return new Response('Hello')
+//     return Response.json({ msg: 'Hello' })
+//   },
+// })
+
+export default {
+  async fetch(request: Request, server: Server) {
+    return Response.json({
+      msg: 'Hello',
+    })
   },
-})
+}
