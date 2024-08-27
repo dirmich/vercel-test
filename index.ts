5b76156e3a8a62653f3d582d81e6088b995d69ee
David, Shin <dirmich@gmail.com>
2024-08-27 14:49:35 +0900

    a

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hello via Bun!')
+// console.log('Hello via Bun!')
+
+const server = Bun.serve({
+  port: 3008,
+  fetch(req) {
+    // return new Response('Hello')
+    return Response.json({ msg: 'Hello' })
+  },
+})
